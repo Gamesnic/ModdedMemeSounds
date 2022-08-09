af0e89388102a0719e8f4f72a963ffa7f104f295
Gamesnic <49643487+Gamesnic@users.noreply.github.com>
2022-08-09 13:42:23 +0200

    plugin extensions
    yo mama

---
+++ ModdedMemeSoundsLongMoyai.plugin.js
@@ -0,0 +1,87 @@
+/**
+ * @name ModdedMemeSounds3
+ * @version 4.2.0
+ * @description MemeSounds plugin but with the real moyai sound
+ * @invite yo moma
+ * @author Gamesnic#0621
+ * @authorId 557388558017495046
+ * @authorLink https://github.com/Gamesnic/
+ * @source https://github.com/Gamesnic/ModdedMemeSounds/blob/main/ModdedMemeSounds.plugin.js
+ * @updateUrl https://github.com/Gamesnic/ModdedMemeSounds/blob/main/ModdedMemeSounds.plugin.js
+ */
+
+module.exports = (() => {
+	
+	/* Configuration */
+	const config = {info: {name: "Modded Meme Sounds Long Moyai", authors: [{name: "Gamesnic#0621", discord_id: "1111111111111111", github_username: "Gamesnic", twitter_username: "someone"},{name: "ライト#8888", discord_id: "111111111111111111", github_username: "someoneelse"}], version: "4.2.0", description: "MemeSounds but with real Moyai sound", github: "https://github.com/Gamesnic/ModdedMemeSounds/blob/main/ModdedMemeSounds.plugin.js", github_raw: "https://github.com/Gamesnic/ModdedMemeSounds/main/ModdedMemeSounds.plugin.js"}, defaultConfig: [{id: "setting", name: "Sound Settings", type: "category", collapsible: true, shown: true, settings: [{id: "LimitChan", name: "Limit to the current channel only.", note: "When enabled, sound effects will only play within the currently selected channel.", type: "switch", value: true}, {id: "delay", name: "Sound effect delay.", note: "The delay in miliseconds between each sound effect.", type: "slider", value: 200, min: 10, max: 1000, renderValue: v => Math.round(v) + "ms"}, {id: "volume", name: "Sound effect volume.", note: "How loud the sound effects will be.", type: "slider", value: 1, min: 0.01, max: 1, renderValue: v => Math.round(v*100) + "%"}]}], changelog: [{title: "New Stuff", items: ["Added the vine boom sound effect when :moyai: is sent into chat.", "Thanks to Orangenal name#9280 for adding vine boom!"]}]};
+
+	/* Library Stuff */
+	return !global.ZeresPluginLibrary ? class {
+		constructor() { this._config = config; }
+        getName() {return config.info.name;}
+        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
+        getDescription() {return config.info.description;}
+        getVersion() {return config.info.version;}
+		load() {BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {confirmText: "Download Now", cancelText: "Cancel", onConfirm: () => {require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (err, res, body) => {if (err) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9"); await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));});}});}
+		start() { }
+		stop() { }
+	} : (([Plugin, Api]) => {
+
+		const plugin = (Plugin, Api) => { try {
+			
+			/* Constants */
+			const {DiscordModules: {Dispatcher, SelectedChannelStore}} = Api;
+			const sounds = [
+				{re: /no?ice/gmi, file: "noice.mp3", duration: 600},
+				{re: /bazinga/gmi, file: "bazinga.mp3", duration: 550},
+				{re: /bruh/gmi, file: "bruh.mp3", duration: 470},
+				{re: /moyai/gmi, file: "longmoyai.mp3", duration: 100},
+				{re: /🗿/gmi, file: "longmoyai.mp3", duration: 100},
+			];
+
+			/* Double message event fix */
+			let lastMessageID = null;
+
+			/* Meme Sounds Class */
+			return class ModdedMemeSounds extends Plugin {
+				constructor() {
+					super();
+				}
+
+				getSettingsPanel() {
+					return this.buildSettingsPanel().getElement();
+				}
+	
+				onStart() {
+					Dispatcher.subscribe("MESSAGE_CREATE", this.messageEvent);
+				}
+				
+				messageEvent = async ({ channelId, message, optimistic }) => {
+					if (this.settings.setting.LimitChan && channelId != SelectedChannelStore.getChannelId())
+						return;
+
+					if (!optimistic && lastMessageID != message.id) {
+						lastMessageID = message.id;
+						let queue = new Map();
+						for (let sound of sounds) {
+							for (let match of message.content.matchAll(sound.re))
+								queue.set(match.index, sound);
+						}
+						for (let sound of [...queue.entries()].sort((a, b) => a[0] - b[0])) {
+							let audio = new Audio("https://github.com/Gamesnic/ModdedMemeSounds/raw/main/MemeSounds/Sounds/"+sound[1].file);
+							audio.volume = this.settings.setting.volume;
+							audio.play();
+							await new Promise(r => setTimeout(r, sound[1].duration+this.settings.setting.delay));
+						}
+					}
+					
+				};
+				
+				onStop() {
+					Dispatcher.unsubscribe("MESSAGE_CREATE", this.messageEvent);
+				}
+			}
+		} catch (e) { console.error(e); }};
+		return plugin(Plugin, Api);
+	})(global.ZeresPluginLibrary.buildPlugin(config));
+})();
